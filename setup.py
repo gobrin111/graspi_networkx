ac1cf0799ed8e1ecf755ec3e47e195c72024dfb9
manla <mleung8@buffalo.edu>
2024-10-08 01:01:52 -0400

    #86 and #84 Updated necessary packages for pip install and notebook

--- setup.py
+++ setup.py
@@ -1,22 +1,37 @@
-from setup import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
-    name="graspi_netowrkx",
-    version="0.1.0",
-    author="qp",
-    author_email="kazunesashi@gmail.com",
-    description="graSPI implementation using the networkx python package",
-    long_description=open('README.md').read(),  # Long description from your README file
-    long_description_content_type="text/markdown",
-    # url="https://github.com/username/my_project",  # Project URL (optional)
-    packages=find_packages(),       # Automatically find packages in your project
-    install_requires=[              # Dependencies, if any
-        "requests",  # Example dependency
+    name='graspi_networkx',
+    version='0.1.5',
+    packages=find_packages(),
+    install_requires=[
+        'contourpy==1.3.0',
+        'cycler==0.12.1',
+        'fonttools==4.54.1',
+        'kiwisolver==1.4.7',
+        'matplotlib==3.9.2',
+        'networkx>=3.2,<3.4',
+        'numpy>=2.0.0,<2.1',
+        'packaging==24.1',
+        'pandas==2.2.3',
+        'pillow==10.4.0',
+        'pyparsing==3.1.4',
+        'python-dateutil==2.9.0.post0',
+        'pytz==2024.2',
+        'scipy>=1.9.0,<1.14',
+        'six==1.16.0',
+        'tzdata==2024.2'
     ],
-    classifiers=[                   # Metadata for searchability
+    python_requires='>=3.6,<3.10',
+    author="Qi Pan",
+    author_email="mleung8@buffalo.edu",
+    description="A package that utilizes NetworkX functionality for GraSPI",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/gobrin111/graspi_networkx",
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
     ],
-    python_requires='>=3.6',        # Python version requirement
 )
